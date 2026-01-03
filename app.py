33ccf76f09e558fefd92b2c37210a16bb38de665
Jeevana349 <jeevanasai6013@gmail.com>
2026-01-03 18:45:10 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import pickle
+
+model = pickle.load(open('spam.pkl','rb'))
+cv = pickle.load(open('vectorizer.pkl','rb'))
+
+
+st.title("Email Spam Classification application")
+st.write("This is a Machine Learning application to classify emails as spam or ham.")
+user_input= st.text_area("Enter an email to classify",height=150)
+
+if st.button("Classify") :
+    if user_input:
+        data = [user_input]
+        vectorized_data = cv.transform(data).toarray()
+        result = model.predict(vectorized_data)
+        if result[0]==0:
+            st.write("The email is not spam")
+        else:
+            st.write("The email is spam")
+    else:
+        st.write("Please type Email to classify")
+
+st.markdown(
+    """
+    <div style="text-align: center; margin-top: 50px; font-size: 16px; color: grey;">
+        <hr style="margin: 20px 0;">
+        <p><strong>Made with ❤️ by Sai Jeevana</strong></p>
+    </div>
+    """,
+    unsafe_allow_html=True,
+)
